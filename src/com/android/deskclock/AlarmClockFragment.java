2ce2bb0578847efc9842d807d76f99b7d8eb0726
kaiyiz <kaiyiz@codeaurora.org>
2014-11-12 12:35:59 -0800

    DeskClock: Fixed the ringtone name display incorrect when delete music file
    It only displays FALLBACKRING when set alarm ringtone by file explorer,
    is caused by that it not handle the file uri which from file explorer
    in getRingToneTitle method.
    
    Add the judgement to get the file uri and get the right title of alarm
    ringtone.
    
    Change-Id: I666db6e29dce58a3c7f0c78beb4fe5643c22f110
    CRs-Fixed: 546747
    
    Conflicts:
    	res/values/slim_strings.xml
    	src/com/android/deskclock/AlarmClockFragment.java

--- src/com/android/deskclock/AlarmClockFragment.java
+++ src/com/android/deskclock/AlarmClockFragment.java
@@ -50,6 +50,7 @@ import android.transition.Fade;
 import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.transition.TransitionSet;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -79,6 +80,7 @@ import com.android.deskclock.provider.DaysOfWeek;
 import com.android.deskclock.widget.ActionableToastBar;
 import com.android.deskclock.widget.TextTime;
 
+import java.io.File;
 import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.HashSet;
@@ -1181,10 +1183,12 @@ public class AlarmClockFragment extends DeskClockFragment implements
                 if (uri.equals(MultiPlayer.RANDOM_URI)) {
                     title = mContext.getResources().getString(R.string.alarm_type_random);
                 } else {
-                    // This is slow because a media player is created during Ringtone object creation.
-                    Ringtone ringTone = RingtoneManager.getRingtone(mContext, uri);
-                    if (ringTone != null) {
-                        title = ringTone.getTitle(mContext);
+                    if (isRingToneUriValid(uri)) {
+                        // This is slow because a media player is created during Ringtone object creation.
+                        Ringtone ringTone = RingtoneManager.getRingtone(mContext, uri);
+                        if (ringTone != null) {
+                            title = ringTone.getTitle(mContext);
+                        }
                     }
                 }
                 if (title != null) {
@@ -1192,6 +1196,32 @@ public class AlarmClockFragment extends DeskClockFragment implements
                 }
             }
             return title;
+        }
+
+        private boolean isRingToneUriValid(Uri uri) {
+            if (uri.getScheme().contentEquals("file")) {
+                File f = new File(uri.getPath());
+                if (f.exists()) {
+                    return true;
+                }
+            } else if (uri.getScheme().contentEquals("content")) {
+                Cursor cursor = null;
+                try {
+                    cursor = mContext.getContentResolver().query(uri,
+                            new String[] {MediaStore.Audio.Media.TITLE}, null, null, null);
+                    if (cursor != null && cursor.getCount() > 0) {
+                        return true;
+                    }
+                } catch (Exception e) {
+                    LogUtils.e("Get ringtone uri Exception: e.toString=" + e.toString());
+                } finally {
+                    if (cursor != null) {
+                        cursor.close();
+                    }
+                }
+            }
+
+            return false;
         }
 
         public void setNewAlarm(long alarmId) {
